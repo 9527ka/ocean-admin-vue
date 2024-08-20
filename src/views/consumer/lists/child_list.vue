7ae32f15a97d6a9ba6a990ec96ed54211d532f04
lang <lang@qq.com>
2024-08-20 23:12:34 +0800

    用户列表查看下级

--- src/views/consumer/lists/child_list.vue
+++ src/views/consumer/lists/child_list.vue
@@ -28,8 +28,14 @@
             <el-card class="!border-none mt-4" shadow="never">
                 <el-table size="large" v-loading="pager.loading" :data="pager.lists">
                     <el-table-column label="一级用户" prop="user_name" min-width="120" />
+                    <el-table-column label="邀请时间" prop="create_time" min-width="120" />
                     <el-table-column label="二级用户" prop="child_1_name" min-width="130" />
-                    <el-table-column label="邀请时间" prop="create_time" min-width="140" />
+                    <el-form-item label="邀请时间">
+                        <template #default="{ row }">
+                            <span>{{ queryParams.child_1_create_time ? timeFormat(queryParams.child_1_create_time,
+            'yyyy-mm-dd hh:MM:ss') : '' }}</span>
+                        </template>
+                    </el-form-item>
                 </el-table>
                 <div class="flex justify-end mt-4">
                     <pagination v-model="pager" @change="getLists" />
--- src/views/consumer/lists/index.vue
+++ src/views/consumer/lists/index.vue
@@ -2,19 +2,23 @@
     <div>
         <el-card class="!border-none" shadow="never">
             <el-form ref="formRef" class="mb-[-16px]" :model="queryParams" :inline="true">
-                <el-form-item label="用户信息">
-                    <el-input class="w-[280px]" v-model="queryParams.keyword" placeholder="账号/昵称/邮箱/手机号码" clearable
+                <el-form-item label="账号">
+                    <el-input class="w-[280px]" v-model="queryParams.account" placeholder="账号" clearable
                         @keyup.enter="resetPage" />
                 </el-form-item>
-                <el-form-item label="注册时间">
-                    <daterange-picker v-model:startTime="queryParams.create_time_start"
-                        v-model:endTime="queryParams.create_time_end" />
+                <el-form-item label="邮箱">
+                    <el-input class="w-[280px]" v-model="queryParams.email" placeholder="邮箱" clearable
+                        @keyup.enter="resetPage" />
                 </el-form-item>
-                <el-form-item label="注册来源">
-                    <el-select class="w-[280px]" v-model="queryParams.channel">
-                        <el-option v-for="(item, key) in ClientMap" :key="key" :label="item" :value="key" />
-                    </el-select>
+                <el-form-item label="手机号">
+                    <el-input class="w-[280px]" v-model="queryParams.mobile" placeholder="手机号" clearable
+                        @keyup.enter="resetPage" />
                 </el-form-item>
+                <el-form-item label="注册时间" prop="create_time">
+                    <daterange-picker v-model:startTime="queryParams.start_time"
+                        v-model:endTime="queryParams.end_time" />
+                </el-form-item>
+
                 <el-form-item>
                     <el-button type="primary" @click="resetPage">查询</el-button>
                     <el-button @click="resetParams">重置</el-button>
@@ -25,12 +29,12 @@
         </el-card>
         <el-card class="!border-none mt-4" shadow="never">
             <el-table size="large" v-loading="pager.loading" :data="pager.lists">
-                <el-table-column label="账号" prop="account" min-width="100" />
-                <el-table-column label="用户名" prop="real_name" min-width="120" />
-                <el-table-column label="邮箱" prop="email" min-width="130" />
+                <el-table-column label="账号" prop="account" min-width="90" />
+                <el-table-column label="用户名" prop="real_name" min-width="90" />
+                <el-table-column label="邮箱" prop="email" min-width="160" />
                 <el-table-column label="手机号" prop="mobile" min-width="100" />
-                <el-table-column label="积分" prop="points" min-width="120" />
-                <el-table-column label="邀请码" prop="icode" min-width="120" />
+                <el-table-column label="积分" prop="points" min-width="60" />
+                <el-table-column label="邀请码" prop="icode" min-width="90" />
                 <el-table-column label="注册时间" prop="create_time" min-width="140" />
                 <el-table-column label="操作" width="120" fixed="right">
                     <template #default="{ row }">
@@ -50,15 +54,14 @@
 
 import ChildList from './child_list.vue'
 import { usePaging } from '@/hooks/usePaging'
-import { getRoutePath } from '@/router'
 import { getUserList } from '@/api/consumer'
-import { ClientMap } from '@/enums/appEnums'
 
 const queryParams = reactive({
-    keyword: '',
-    channel: '',
-    create_time_start: '',
-    create_time_end: ''
+    account: '',
+    email: '',
+    mobile: '',
+    start_time: '',
+    end_time: ''
 })
 
 const { pager, getLists, resetPage, resetParams } = usePaging({
